bbecf2acbe66700094f91d50f1f3e7e35a7db9d4
hua.fu <hua.fu@msxf.com>
2018-03-08 21:42:00 +0800

    add VRD & 1711.10925

--- SelDir.sh
+++ SelDir.sh
@@ -4,17 +4,19 @@
 SelDir() #copy [beg,end] dirs from src to dst
 {
 	src=$1; dst=$2; beg=$3; end=$4
+	if [ ! -d $dst ]; then mkdir $dst; fi
 	for i in $(ls $src);
 	do
 		#if [ $i -ge $beg ]&&[ $i -le $end ]; then echo "$i" >> List; fi
 		if [ $i -ge $beg ]&&[ $i -le $end ]; then cp -r $src/$i $dst; fi
 	done
-} #0-263,268-473,484-500
+} #0-263,268-473,484-500,221902-226986
 
 src=Tools/CASIA-WebFace
-SelDir $src AI_CV_Train_1 0 263
-SelDir $src AI_CV_Train_2 268 473
-SelDir $src AI_CV_Test_1 484 500
+#SelDir $src AI_CV_Train1 0 263
+#SelDir $src AI_CV_Train2 268 473
+#SelDir $src AI_CV_Test1 484 500
+SelDir $src AI_CV_Test2 221902 226986
 
 python3 Mesh_Face.py &
 
@@ -35,3 +37,5 @@ ReName() #rename *_?.jpg to *_.jpg, then
 src=AI_CV_Train1; dst=AI_CV_Train_1;
 ReName $src $dst &
 zip -rq $dst.zip $dst &
+
+cd $dst;ls *[0-9].jpg >> ../$dst.txt; cd
