9a536d7db48e8a647380873a3eff7e7dc049c7dc
hua.fu <hua.fu@msxf.com>
2018-04-13 15:26:04 +0800

    add JohnPiper

--- PSNR.py
+++ PSNR.py
@@ -7,27 +7,27 @@ from sys import argv
 from math import log10
 
 ##################################################################
-def PSNR(I, K): # require: I.shape == K.shape
+def PSNR(I, K): # Require: I.shape == K.shape
     if type(I)==str: I = cv2.imread(I)
     if type(K)==str: K = cv2.imread(K)
-    Max, ee = 255, 0.0000065025 # ee = Max**2/10**10
-    MSE = cv2.norm(I, K, normType=cv2.NORM_L2)**2 / (K.shape[0]*K.shape[1])
-    PSNR = 10 * math.log10(Max**2/(MSE+ee))
+    assert(I.shape == K.shape) # assert if False
+    h,w,d = K.shape; Max2 = 255**2; ee = Max2*1E-10
+    MSE = cv2.norm(I, K, normType=cv2.NORM_L2)**2 / (w*h)
+    PSNR = 10 * log10(Max2/(MSE+ee))
     return PSNR
 
-def main(*args, **kwargs): # args[0] = Image_Dir
-    im = os.listdir(args[0]);
-    im.sort(); psnr = []
-    for i in range(0,len(im),2):
-        I = cv2.imread(os.path.join(args[0], im[i]))
-        K = cv2.imread(os.path.join(args[0], im[i+1]))
-        psnr.append(PSNR(I,K))
-        print(im[i][:-4], psnr[i//2])
+def PSNR_DIR(*args, **kwargs): # args[0] = Image_DIR
+    DIR = args[0]; im = os.listdir(DIR)
+    im.sort(); psnr = [0]*(len(im)//2)
+    for i in range(len(psnr)):
+        I = cv2.imread(os.path.join(DIR, im[2*i]))
+        K = cv2.imread(os.path.join(DIR, im[2*i+1]))
+        psnr[i] = PSNR(I,K); print(im[2*i][:-4], psnr[i])
     return psnr,im
 
 ##################################################################
 if __name__ == '__main__':
-	main(argv[1])
+    PSNR_DIR(argv[1]) # input DIR
 
 ##################################################################
 # python3 PSNR.py Tools/AI_CV_Test_1 > out.txt &
